d86dc0097748e9b3161cd5cbe6df2e8de735f626
AlexCKHO <alexho.ofl@gmail.com>
2025-08-15 21:49:20 +0100

    Saving work

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,8 +1,11 @@
 // Encapsulates the business logic
 
+using MediatR;
+
 namespace Catalog.API.Products.CreateProduct;
 
 // Data that needed to create a new product
+// record is just a special kind of class introduced in C# 9 that is designed for immutable data and value-based equality.
 public record CreateProductCommand(
     Guid Id,
     string Name,
@@ -10,10 +13,21 @@ public record CreateProductCommand(
     string Description,
     string ImageFile,
     decimal Price
-);
+) : IRequest<CreateProductResult>;
 
 // Response result object in this case we only return the ID
 // Once successfully create product, then return that product ID
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler { }
+internal class CreateProductCommandHandler
+    : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(
+        CreateProductCommand request,
+        CancellationToken cancellationToken
+    )
+    {
+        // Business logic to create a product
+        return Task.FromResult(new CreateProductResult(request.Id));
+    }
+} 
