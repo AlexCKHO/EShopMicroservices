4ce88b594cdbfc842f51b3181c4a592ad73a2b7c
Alex Ho <cho@spartaglobal.com>
2025-08-23 20:27:31 +0100

    Saving work

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,4 +1,5 @@
 // Encapsulates the business logic
+
 namespace Catalog.API.Products.CreateProduct;
 
 // Data that needed to create a new product
@@ -15,7 +16,22 @@ public record CreateProductCommand(
 // Once successfully create product, then return that product ID
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler(IDocumentSession session)
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+    }
+
+}
+
+
+
+
+internal class CreateProductCommandHandler(IDocumentSession session, IValidator<CreateProductCommand> validator)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(
@@ -26,6 +42,15 @@ internal class CreateProductCommandHandler(IDocumentSession session)
         //Create product entity from command object
         //Save to database
         // return CreateProdutResult result
+
+        var result = await validator.ValidateAsync( command , cancellationToken);
+
+        var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
+
+        if (errors.Any())
+        {
+            throw new ValidationException(errors.FirstOrDefault());
+        }
 
         var product = new Product
         {
