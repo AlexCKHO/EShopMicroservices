d0d1c83f60ef1dffb9c6af02135b74705fa7da67
Alex Ho <cho@spartaglobal.com>
2025-08-28 22:48:52 +0100

    Saving work

---
+++ src/Services/Basket/Basket.API/Data/BasketRepository.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Basket.API.Data
+{
+    public class BasketRepository : IBasketRepository
+    {
+  
+        public Task<ShoppingCart> GetBasket(int userName, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellation = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Data/IBasketRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Basket.API.Data
+{
+    public interface IBasketRepository
+    {
+
+        Task<ShoppingCart> GetBasket(int userName, CancellationToken cancellationToken = default);
+
+        Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellation = default);
+
+        Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default);
+
+    }
+}
