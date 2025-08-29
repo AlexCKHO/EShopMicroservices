437f8ae6fa7e2a9047cb209479cb79edc232fcb4
Alex Ho <cho@spartaglobal.com>
2025-08-29 18:17:15 +0100

    Saving work

--- src/Services/Basket/Basket.API/Data/IBasketRepository.cs
+++ src/Services/Basket/Basket.API/Data/IBasketRepository.cs
@@ -3,7 +3,7 @@
     public interface IBasketRepository
     {
 
-        Task<ShoppingCart> GetBasket(int userName, CancellationToken cancellationToken = default);
+        Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default);
 
         Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellation = default);
 
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -11,6 +11,13 @@ builder.Services.AddMediatR(config =>
     }
     );
 
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database"));
+
+}
+).UseLightweightSessions();
+
 var app = builder.Build();
 
 app.MapCarter();
