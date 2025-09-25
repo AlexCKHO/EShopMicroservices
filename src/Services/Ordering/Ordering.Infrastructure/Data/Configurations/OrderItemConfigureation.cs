aeedf8aa0dd233a2406ca73b5f43d7ac0b6cc24c
Alex Ho <cho@spartaglobal.com>
2025-09-25 23:11:57 +0100

    Saving work

--- src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfigureation.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfigureation.cs
@@ -15,6 +15,14 @@ namespace Ordering.Infrastructure.Data.Configurations
             builder.Property(oi => oi.Id).HasConversion(
                 orderItemId => orderItemId.Value,
                 dbId => OrderItemId.Of(dbId));
+
+            builder.HasOne<Product>()
+                .WithMany()
+                .HasForeignKey(oi => oi.ProductId);
+
+            builder.Property(oi => oi.Quantity).IsRequired();
+            builder.Property(oi => oi.Price).IsRequired();
+
         }
     }
 }
