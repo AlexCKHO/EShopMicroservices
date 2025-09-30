fdc057584526798bfe365012f61dc06868b416b8
Alex Ho <cho@spartaglobal.com>
2025-09-30 23:03:44 +0100

    Saving work

--- src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -4,3 +4,5 @@ global using Microsoft.Extensions.DependencyInjection;
 global using Ordering.Infrastructure.Data;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
+global using Ordering.Domain.Abstractions;
+global using Ordering.Infrastructure.Data.Interceptors;
