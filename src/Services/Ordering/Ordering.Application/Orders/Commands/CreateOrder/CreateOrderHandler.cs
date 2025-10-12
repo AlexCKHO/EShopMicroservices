27e7a8e8c3e3013d400790f2037a5fb3a18be626
Alex Ho <cho@spartaglobal.com>
2025-10-12 23:13:22 +0100

    Saving work

--- src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -1,4 +1,8 @@
 ﻿using BuildingBlocks.CQRS;
+using Ordering.Application.Data;
+using Ordering.Application.Dtos;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,14 +11,43 @@ using System.Threading.Tasks;
 
 namespace Ordering.Application.Orders.Commands.CreateOrder
 {
-    public class CreateOrderHandler : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+    public class CreateOrderHandler(IApplicationDbContext dbContext)
+        : ICommandHandler<CreateOrderCommand, CreateOrderResult>
     {
-        public Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        public async Task<CreateOrderResult> Handle(CreateOrderCommand command, CancellationToken cancellationToken)
+        {
+            // create Order entity from command object
+            var order = CreateNewOrder(command.Order);
+
+            //save to database
+            dbContext.Orders.Add(order);
+            await dbContext.SaveChangeAsync(cancellationToken);
+
+            return new CreateOrderResult(order.Id.Value);
+
+        }
+
+        // The purpose of this method is to convert Dto to table object
+        private Order CreateNewOrder(OrderDto orderDto)
         {
 
+            var shippingAddress = Address.Of(orderDto.ShippingAddress.FirstName, orderDto.ShippingAddress.LastName, orderDto.ShippingAddress.EmailAddress, orderDto.ShippingAddress.AddressLine, orderDto.ShippingAddress.Country, orderDto.ShippingAddress.State, orderDto.ShippingAddress.ZipCode);
+            var billingAddress = Address.Of(orderDto.BillingAddress.FirstName, orderDto.BillingAddress.LastName, orderDto.BillingAddress.EmailAddress, orderDto.BillingAddress.AddressLine, orderDto.BillingAddress.Country, orderDto.BillingAddress.State, orderDto.BillingAddress.ZipCode);
 
+            var newOrder = Order.Create(
+                    id: OrderId.Of(Guid.NewGuid()),
+                    customerId: CustomerId.Of(orderDto.CustomerId),
+                    orderName: OrderName.Of(orderDto.OrderName),
+                    shippingAddress: shippingAddress,
+                    billingAddress: billingAddress,
+                    payment: Payment.Of(orderDto.Payment.CardName, orderDto.Payment.CardNumber, orderDto.Payment.Expiration, orderDto.Payment.Cvv, orderDto.Payment.PaymentMethod)
+                    );
 
-            throw new NotImplementedException();
+            foreach (var orderItemDto in orderDto.OrderItems)
+            {
+                newOrder.Add(ProductId.Of(orderItemDto.ProductId), orderItemDto.Quantity, orderItemDto.Price);
+            }
+            return newOrder;
 
         }
 
