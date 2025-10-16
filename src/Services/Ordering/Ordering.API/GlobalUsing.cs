c3696fd1c33a0b91f905d9c6c3e0385144e8d629
Alex Ho <cho@spartaglobal.com>
2025-10-16 22:45:54 +0100

    Saving work

--- src/Services/Ordering/Ordering.API/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.API/GlobalUsing.cs
@@ -2,3 +2,8 @@
 global using Ordering.Application;
 global using Ordering.Infrastructure;
 global using Ordering.Infrastructure.Data.Extenstions;
+global using Carter;
+global using Mapster;
+global using MediatR;
+global using Ordering.Application.Dtos;
+global using Ordering.Application.Orders.Commands.UpdateOrder;
