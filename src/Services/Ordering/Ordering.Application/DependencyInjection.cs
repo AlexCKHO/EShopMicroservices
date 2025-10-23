3b73b2734183e20c1630f21d660630aa7fd791be
Alex Ho <cho@spartaglobal.com>
2025-10-24 00:44:45 +0100

    Saving work

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using BuildingBlocks.Behaviors;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Reflection;
@@ -9,22 +10,18 @@ namespace Ordering.Application
    public static class DependencyInjection
     {
 
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services) 
+        public static IServiceCollection AddApplicationServices
+         (this IServiceCollection services, IConfiguration configuration)
         {
-
             services.AddMediatR(config =>
             {
-
-                //“Scan this assembly(the current project — e.g., Ordering.Application) and automatically 
-                //register all classes that implement any MediatR interfaces.”
                 config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-                config.AddBehavior(typeof(ValidationBehavior<,>));
-                config.AddBehavior(typeof(LoggingBehavior<,>));
-
+                config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+                config.AddOpenBehavior(typeof(LoggingBehavior<,>));
             });
 
-                return services;
-
+          
+            return services;
         }
     }
 }
