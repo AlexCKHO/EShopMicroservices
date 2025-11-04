12958ad684e136ae24c20099a30d9d14d637808c
Alex Ho <cho@spartaglobal.com>
2025-11-04 22:13:09 +0000

    Saving work

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,9 +7,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy")); ;
 
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+
+
+});
+
+
 var app = builder.Build();
 
 // Cpmfigure the HTTP request pipeline.
+
+app.UseRateLimiter();
+
+
 app.MapReverseProxy();
 
 app.Run();
