f1ab8380b6660116c40ffcfa8425b7a256a7789b
AlexCKHO <alexho.ofl@gmail.com>
2025-12-05 22:45:12 +0000

    Saving work

--- src/WebApps/Shopping.Web/GlobalUsing.cs
+++ src/WebApps/Shopping.Web/GlobalUsing.cs
@@ -1,7 +1,7 @@
 ﻿global using Shopping.Web.Models.Catalog;
 global using Shopping.Web.Models.Basket;
 global using Shopping.Web.Models.Ordering;
-
 global using Shopping.Web.Services;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Mvc.RazorPages;
+global using Refit;
--- src/WebApps/Shopping.Web/Program.cs
+++ src/WebApps/Shopping.Web/Program.cs
@@ -1,7 +1,15 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddRefitClient<ICatalogService>()
+    .ConfigureHttpClient(c =>
+    {
+        c.BaseAddress =
+            new Uri(builder.Configuration["ApiSettings:CatalogServiceUrl"]!);
+    });
 
 var app = builder.Build();
 
